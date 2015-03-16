4db9e975c5ef582fc2d58a439c52253fe2259b3a
unknown <janilychen@janilychen-PC1.tencent.com>
2015-03-16 11:09:26 +0800

    update

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 The JavaScript SVG path animation library for the modern web. 
+一个用来使用SVG的路径(path)来制作动画效果的插件
---
+++ cobrasvg.js
@@ -0,0 +1,14 @@
+/**
+ * cobrasvg
+ *
+ * 
+ * 一个用来使用SVG的路径(path)来制作动画效果的插件
+ *
+ * Licensed under the MIT license.
+ * http://www.opensource.org/licenses/mit-license.php
+ *
+ * Copyright 2015, Call Me janily
+ * 
+ */
+
+ 
